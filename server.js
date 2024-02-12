4e3600d88057ca65c3b9d21c933eda1e753f1da4
ridhoakbar8977 <ridho8977@gmail.com>
2024-02-12 08:10:43 +0700

    update

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+   res.header("Access-Control-Allow-Headers", "Content-Type");
+   res.header("Access-Control-Allow-Credentials", "true");
+   next();
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 const db = process.env.DB;
@@ -14,14 +22,14 @@ const db = process.env.DB;
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", authRoutes);
+app.use("/api", eventRoutes);
+
 mongoose.set("strictQuery", false);
 mongoose.connect(db, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
 });
-
-app.use("/auth", authRoutes);
-app.use("/api", eventRoutes);
 
 app.listen(port, () => {
    console.log(`Server berjalan di http://localhost:${port}`);
