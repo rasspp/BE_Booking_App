135e87f58cd0b1356070d4f521ed3912e767ff30
ridhoakbar8977 <ridho8977@gmail.com>
2024-02-19 12:30:47 +0700

    add origin

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ const db = process.env.MONGO_URI;
 app.use(cors());
 
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+   const allowedOrigins = ["https://www.zuppasoup.my.id"];
+   const origin = req.headers.origin;
+
+   if (allowedOrigins.includes(origin)) {
+      res.header("Access-Control-Allow-Origin", origin);
+   }
+
    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
    res.header("Access-Control-Allow-Headers", "Content-Type");
    res.header("Access-Control-Allow-Credentials", "true");
