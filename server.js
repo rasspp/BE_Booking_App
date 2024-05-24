53ecc015ca7084aeb2ef0a8b8f30ce2b6e68304e
ridhoakbar8977 <ridho8977@gmail.com>
2024-05-24 09:49:35 +0700

    update: add page default

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
-import docRoutes from "./routes/docRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -34,7 +33,10 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/api", eventRoutes);
-app.use("/api", docRoutes);
+
+app.get("/", (req, res) => {
+   res.send("<h1>Server telah aktif dan berjalan di http://localhost:" + port + "</h1>");
+});
 
 mongoose.set("strictQuery", false);
 mongoose.connect(db, {
